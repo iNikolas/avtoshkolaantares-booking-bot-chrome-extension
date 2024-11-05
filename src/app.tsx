c739b10e966c4f03798e58f29a92475f19f45152
iNikolas <realkolos1@gmail.com>
2024-11-05 13:53:09 +0200

    bugfix/ add app

---
+++ src/app.tsx
@@ -0,0 +1,13 @@
+import { BookingForm } from "@screens";
+
+function App() {
+  return (
+    <main className="prose min-w-max p-4">
+      <h1 className="text-center">Antares Booking Bot</h1>
+
+      <BookingForm />
+    </main>
+  );
+}
+
+export default App;
